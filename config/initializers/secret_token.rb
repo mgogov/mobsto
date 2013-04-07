47e1a29ce818871e9bddd8166d6b393fa6e6cd02
Martin Gogov <mgogov@gmail.com>
2013-04-07 12:25:58 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobsto::Application.config.secret_token = '17bef3075e8e56b5b52c0ca38d55f444c4d833caeca377339676925fd704c4b2ea112e3f3b0c87c2701a5c211c71b73e9c43281e11f18f1ccc6fbe4de99c4568'
